40f23b23714f39287d2924ec43696594bc313050
WingsJ <mail@wings-j.cn>
2026-06-08 20:31:35 +0800

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue';
+import UnpluginElementPlus from 'unplugin-element-plus/vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [Vue(), UnpluginElementPlus]
+});
